51508aaaa362a1f690074df1f43c5aece71e45e7
Adrien L <alopez@heliostech.hk>
2013-12-22 17:13:16 +0100

    initial project version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Popapps::Application.config.secret_key_base = '6069f7b6ce837cb8349852d1402410fb5cbc3b8d63dc20d2ef1a4ea911c082d6b5ce7fc3bb7ee7897c67ca0b1ce14e6b3c9fd621f5f9db84a0326473a6ebeb50'
